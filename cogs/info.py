698d4860e199b27007c9d66af7802487eb23af68
RuiL1904 <rui.lilokas2003@hotmail.com>
2021-08-29 23:46:44 +0100

    Added some versions' info

--- cogs/info.py
+++ cogs/info.py
@@ -1,4 +1,6 @@
+import platform
 import discord
+from discord import client
 from discord.ext import commands
 import datetime
 
@@ -22,7 +24,23 @@ class info(commands.Cog):
 
         embed.add_field(
             name = 'Data de Lançamento:',
-            value = '29 de Dezembro de 2020'
+            value = '29 de Dezembro de 2020',
+            inline = False
+        )
+
+        embed.add_field(
+            name = 'Python Version',
+            value = platform.python_version()
+        )
+
+        embed.add_field(
+            name = 'Discord.py Version',
+            value = discord.__version__
+        )
+
+        embed.add_field(
+            name = 'Latência',
+            value = (f'{int(round((self.client.latency * 1000), 0))} ms')
         )
 
         embed.set_footer(text = (f'Request by {context.message.author.name}'))
