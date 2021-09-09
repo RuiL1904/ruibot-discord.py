ff845960c73c46d2f56762959221d7f6c064d6bc
RuiL1904 <rui.lilokas2003@hotmail.com>
2021-09-09 21:07:53 +0100

    Removed command handler and added support for jishaku

--- launcher.py
+++ launcher.py
@@ -15,7 +15,7 @@ with open(r'data/cmd_handler.json') as file:
 
 prefix = '.'
 
-client = commands.Bot(command_prefix = commands.when_mentioned_or(prefix), help_command = None, owner_id = vars['MY_ID'])
+client = commands.Bot(command_prefix = commands.when_mentioned_or(prefix), help_command = None, owner_id = int(vars['MY_ID']))
 
 timestamp = datetime.utcnow()
 
@@ -25,6 +25,9 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
         cogs.append(filename[:-3])
+
+# Load jishaku
+client.load_extension('jishaku')
 
 # Bot ready event
 @client.event
@@ -38,103 +41,6 @@ async def on_ready():
         print(f'Os seguintes comandos foram carregados: {cogs}')
     
     await client.change_presence(status = discord.Status.do_not_disturb, activity = discord.Game(name = '.help'))
-
-# Command handler
-@client.command(name = 'load')
-@commands.has_role('Developer')
-async def load(context, cog):
-    
-    if cog in data['loaded']:
-        embed = discord.Embed(
-            title = 'ERRO',
-            description = (f'```O comando {cog} já está ativo!```'),
-            color = discord.Color(0xcc3300)
-        )
-
-        embed.set_footer(text = (f'Requested by {context.message.author.name}'))
-        embed.timestamp = timestamp
-        
-        await context.reply(embed = embed)
-    
-    elif cog in data['unloaded']:
-        client.load_extension(f'cogs.{cog}')
-
-        embed = discord.Embed(
-            title = 'AVISO',
-            description = (f'```O comando {cog} foi ativado com sucesso!```'),
-            color = discord.Color(0xcc3300)
-        )
-
-        embed.set_footer(text = (f'Requested by {context.message.author.name}'))
-        embed.timestamp = timestamp
-        
-        await context.reply(embed = embed)
-        
-        data['unloaded'].remove(cog) 
-        data['loaded'].append(cog)              
-  
-@client.command(name = 'unload')
-@commands.has_role('Developer')
-async def unload(context, cog):
-    
-    if cog in data['unloaded']:
-        embed = discord.Embed(
-            title = 'ERRO',
-            description = (f'```O comando {cog} já está inativo!```'),
-            color = discord.Color(0xcc3300)
-        )
-
-        embed.set_footer(text = (f'Requested by {context.message.author.name}'))
-        embed.timestamp = timestamp
-        
-        await context.reply(embed = embed)
-        
-    elif cog in data['loaded']:
-        client.unload_extension(f'cogs.{cog}')
-
-        embed = discord.Embed(
-            title = 'AVISO',
-            description = (f'```O comando {cog} foi desativado com sucesso!```'),
-            color = discord.Color(0xcc3300)
-        )
-
-        embed.set_footer(text = (f'Requested by {context.message.author.name}'))
-        embed.timestamp = timestamp
-        
-        await context.reply(embed = embed)
-        
-        data['loaded'].remove(cog)
-        data['unloaded'].append(cog)
-
-# List each command
-@client.command(name = 'list')
-@commands.has_role('Developer')
-async def list(context):
-    
-    loaded = data['loaded']
-    unloaded = data['unloaded']
-
-    embed = discord.Embed(
-        title = 'Lista de comandos',
-        description = '\u200b',
-        color = discord.Color(0xcc3300)
-    )
-
-    embed.add_field(
-        name = 'Comandos ativos',
-        value = loaded,
-    )
-
-    embed.add_field(
-        name = 'Comandos inativos',
-        value = unloaded,
-        inline = False
-    )
-
-    embed.set_footer(text = (f'Requested by {context.message.author.name}'))
-    embed.timestamp = timestamp
-
-    await context.reply(embed = embed)
 
 # Error handler
 @client.event
@@ -155,12 +61,11 @@ async def on_command_error(context, error, member = discord.Member):
         )
 
     elif isinstance(error, commands.UserInputError):
-        if not isinstance(error, commands.MissingRequiredArgument):
-            embed = discord.Embed(
-                title = 'ERRO',
-                description = (f'```Algo está mal no teu input... \nRetifica-o e tenta novamente!```'),
-                color = discord.Color(0xcc3300)
-            )           
+        embed = discord.Embed(
+            title = 'ERRO',
+            description = (f'```Algo está mal no teu input... \nRetifica-o e tenta novamente!```'),
+            color = discord.Color(0xcc3300)
+        )           
     
     elif isinstance(error, commands.MissingPermissions):
         embed = discord.Embed(
@@ -180,6 +85,13 @@ async def on_command_error(context, error, member = discord.Member):
         embed = discord.Embed(
             title = 'ERRO',
             description = '```Infelizmente não tenho permissões para fazer isso...```',
+            color = discord.Color(0xcc3300)
+        )
+    
+    elif isinstance(error, commands.TooManyArguments):
+        embed = discord.Embed(
+            title = 'ERRO',
+            description = '```Existem aí argumentos a mais... \n Digita .help para mais info!```',
             color = discord.Color(0xcc3300)
         )
     
