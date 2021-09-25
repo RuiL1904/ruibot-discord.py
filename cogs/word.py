bf549259b64c576f3fc0b0324f0cc802f1debd35
Rui <rui.lilokas2003@hotmail.com>
2021-09-25 22:41:28 +0100

    Create .word
    .word consists in a command that gets 3 random words from a random words API (it was a friend request, actually)

---
+++ cogs/word.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import nextcord as discord
+from nextcord.ext import commands
+import aiohttp
+
+class word(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.command(name = 'word')
+    async def word(self, context, count):
+        timestamp = datetime.utcnow()
+
+        # Get data from the API
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'https://random-word-api.herokuapp.com/word?number={count}') as response:
+
+                data = await response.json()
+                
+                # Embed sent by the bot 
+                embed = discord.Embed(
+                    title = f'Aqui tens {count} palavras',
+                    description = (', '.join(data)),
+                    color = discord.Color(0xcc3300)
+                )
+
+                await context.reply(embed = embed)
+
+def setup(client):
+    client.add_cog(word(client))
